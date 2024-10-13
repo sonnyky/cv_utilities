93d4e9554ed47dc583a3a711d1dc650788385b5e
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2024-10-13 21:27:43 +0900

    Update plane_detector_main.py
    capture mean depth of ROI pixels

--- plane_detection_oakd/plane_detector_main.py
+++ plane_detection_oakd/plane_detector_main.py
@@ -1,6 +1,7 @@
 from cam_setup import CameraDevice
 import depthai as dai
 import cv2
+import numpy as np
 
 device = dai.Device()
 calibData = device.readCalibration()
@@ -10,12 +11,52 @@ camSetup.fix_focus(calibData)
 camSetup.linking()
 pipeline = camSetup.get_pipeline()
 
+color = (255, 255, 255)
+
 with device:
     device.startPipeline(pipeline)
 
-    rgbWindowName = "rgb"
-    depthWindowName = "depth"
-    blendedWindowName = "rgb-depth"
-    cv2.namedWindow(rgbWindowName)
-    cv2.namedWindow(depthWindowName)
-    cv2.namedWindow(blendedWindowName)
+    depthQueue = device.getOutputQueue(name="depth", maxSize=4, blocking=False)
+    spatialCalcQueue = device.getOutputQueue(name="spatialData", maxSize=4, blocking=False)
+    spatialCalcConfigInQueue = device.getInputQueue("spatialCalcConfig")
+
+    while True:
+        inDepth = depthQueue.get()  # Blocking call, will wait until a new data has arrived
+
+        depthFrame = inDepth.getFrame()  # depthFrame values are in millimeters
+
+        depth_downscaled = depthFrame[::4]
+        if np.all(depth_downscaled == 0):
+            min_depth = 0  # Set a default minimum depth value when all elements are zero
+        else:
+            min_depth = np.percentile(depth_downscaled[depth_downscaled != 0], 1)
+        max_depth = np.percentile(depth_downscaled, 99)
+        depthFrameColor = np.interp(depthFrame, (min_depth, max_depth), (0, 255)).astype(np.uint8)
+        depthFrameColor = cv2.applyColorMap(depthFrameColor, cv2.COLORMAP_HOT)
+
+        spatialData = spatialCalcQueue.get().getSpatialLocations()
+        for depthData in spatialData:
+            roi = depthData.config.roi
+            roi = roi.denormalize(width=depthFrameColor.shape[1], height=depthFrameColor.shape[0])
+            xmin = int(roi.topLeft().x)
+            ymin = int(roi.topLeft().y)
+            xmax = int(roi.bottomRight().x)
+            ymax = int(roi.bottomRight().y)
+
+            depthMin = depthData.depthMin
+            depthMax = depthData.depthMax
+
+            fontType = cv2.FONT_HERSHEY_TRIPLEX
+            cv2.rectangle(depthFrameColor, (xmin, ymin), (xmax, ymax), color, 1)
+            cv2.putText(depthFrameColor, f"X: {int(depthData.spatialCoordinates.x)} mm", (xmin + 10, ymin + 20),
+                        fontType, 0.5, color)
+
+            cv2.putText(depthFrameColor, f"Y: {int(depthData.spatialCoordinates.y)} mm", (xmin + 10, ymin + 35),
+                        fontType, 0.5, color)
+            cv2.putText(depthFrameColor, f"Z: {int(depthData.spatialCoordinates.z)} mm", (xmin + 10, ymin + 50),
+                        fontType, 0.5, color)
+            # Show the frame
+        cv2.imshow("depth", depthFrameColor)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
